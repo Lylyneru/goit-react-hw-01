dbc8e25c94b496806f7daf611a7bf9cffd92846d
Lylyneru <lyly777@gmail.com>
2025-01-13 14:25:00 +0200

    add css

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "modern-normalize";
-import "./index.css";
+import "./style.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
